969969464058d97a6debbb104863a05c05bd3079
uranvex <162054385+uranvex@users.noreply.github.com>
2024-10-12 01:30:51 -0700

    Update ConfigUtil.java

--- src/main/java/uv/uranvex/uvlib/utils/ConfigUtil.java
+++ src/main/java/uv/uranvex/uvlib/utils/ConfigUtil.java
@@ -1,17 +1,17 @@
 package uv.uranvex.uvlib.utils;
 
-import uv.uranvex.uvlib.UVLib;
+import uv.uranvex.uvlib.Main;
 
 public class ConfigUtil {
     public static String getString(String path) {
-        return HexUtil.color(UVLib.instance.getConfig().getString(path));
+        return HexUtil.color(Main.instance.getConfig().getString(path));
     }
 
     public static int getInt(String path) {
-        return UVLib.instance.getConfig().getInt(path);
+        return Main.instance.getConfig().getInt(path);
     }
 
     public static  Boolean getBoolean(String path){
-        return UVLib.instance.getConfig().getBoolean(path);
+        return Main.instance.getConfig().getBoolean(path);
     }
 }
