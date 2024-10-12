0fe27c60dda5c217f4647d78bbd3975a371acc75
uranvex <162054385+uranvex@users.noreply.github.com>
2024-10-12 01:29:39 -0700

    Update and rename UVLib.java to Main.java

--- src/main/java/uv/uranvex/uvlib/Main.java
+++ src/main/java/uv/uranvex/uvlib/Main.java
@@ -2,8 +2,8 @@ package uv.uranvex.uvlib;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class UVLib extends JavaPlugin {
-    public static UVLib instance;
+public final class Main extends JavaPlugin {
+    public static Main instance;
 
     @Override
     public void onEnable() {
